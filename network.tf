f14938463fc69d3750fe933a6d7a09e124bc8237
changsb78 <chang.son@spintech.co.kr>
2021-10-08 06:57:23 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/changsb/network/azurerm"
+  version = "3.5.0"
+
+  resource_group_name = "changsb"
+}
